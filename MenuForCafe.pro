bf1f30cf4b7392a2b038d4697db94e20c385ad70
Dmytro Mykuliak <dmytro.mykuliak@vakoms.com.ua>
2018-05-20 10:38:54 +0300

    Moved project to my github account. Implemented start UI page, and core part for the printing menu in the console.

--- MenuForCafe.pro
+++ MenuForCafe.pro
@@ -1,5 +1,6 @@
 TEMPLATE = app
 QMAKE_CXXFLAGS += -std=c++17
+<<<<<<< HEAD
 CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
@@ -15,3 +16,19 @@ HEADERS += \
     menuitem.h \
     menusection.h \
     abstractmenuvisitor.h
+=======
+QT += core gui widgets
+
+include($$PWD/core/MenuForCafeCore.pri)
+
+SOURCES += \
+    $$PWD/main.cpp \
+    $$PWD/mainwindow.cpp
+
+HEADERS += \
+    $$PWD/mainwindow.h
+
+FORMS += \
+    $$PWD/mainwindow.ui
+
+>>>>>>> d580f1c... Moved project to my github account. Implemented start UI page, and core part for the printing menu in the console.
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 #include "abstractmenuitem.h"
 #include "menuitem.h"
@@ -8,6 +9,21 @@
 
 int main()
 {
+=======
+#include <QApplication>
+#include "mainwindow.h"
+//#include <iostream>
+//#include "abstractmenuitem.h"
+//#include "menuitem.h"
+//#include "menusection.h"
+
+//#include <memory>
+//#include <utility>
+
+int main(int argc, char** argv)
+{
+    /*
+>>>>>>> d580f1c... Moved project to my github account. Implemented start UI page, and core part for the printing menu in the console.
     auto drinks = std::make_unique<MenuSection>("drinks");
     drinks->addItem(std::make_unique<MenuItem>("Beer", 0.0));
     drinks->addItem(std::make_unique<MenuItem>("water", 0.0));
@@ -19,4 +35,13 @@ int main()
     MenuSection menu("MENU");
     menu.addItem(std::move(drinks));
     menu.addItem(std::move(food));
+<<<<<<< HEAD
+=======
+    */
+    QApplication app(argc, argv);
+    MainWindow mainwindow;
+    mainwindow.show();
+    return app.exec();
+
+>>>>>>> d580f1c... Moved project to my github account. Implemented start UI page, and core part for the printing menu in the console.
 }
